a8ae6357d3f9a099731c912f6227bb0dd225d3ec
wangtongze <wangtongze@yahoo.com>
2015-12-07 21:41:28 -0800

    add-libsodium

--- install.sh
+++ install.sh
@@ -14,5 +14,27 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "Root Permission Granted"
+echo "Do you want to enable chacha20 and salsa20? (Require libsodium)??"
+echo "Please type [Y] or [N] and press [ENTER] to continue!!"
+read libins
+
+if [ "$libins" == "Y" ];
+then
+  echo "We will install libsodium."
+  #install libsoduim
+  wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+  tar -xvf LATEST.tar.gz
+  cd libsodium*/
+  apt-get install -y build-essential	
+  ./configure && make && make install
+  ldconfig
+  cd ..
+  rm LATEST.tar.gz
+  rm -rf libsodium*/
+  echo "libsoduim installed successfully."
+else
+  echo "We'll NOT going to install libsodium."
+fi
+
 
 exit 0
